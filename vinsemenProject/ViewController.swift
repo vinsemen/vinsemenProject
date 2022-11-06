c049461ed409d0f5228e05c6e3522f0b3cf23032
Semen Vinnikov <semvinnikov@yandex.ru>
2022-11-07 00:20:29 +0300

    Initial Commit

---
+++ vinsemenProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vinsemenProject
+//
+//  Created by Семён Винников on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
